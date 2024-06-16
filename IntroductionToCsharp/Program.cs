fe9fb15c8e972e5c34c0efbf57047a797c47f0fc
mayur <mayur@DESKTOP-217J50E>
2024-06-16 19:58:08 +0530

    Add project files.

---
+++ IntroductionToCsharp/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace IntroductionToCsharp
+{
+    class Program
+    {
+        #region Introduction
+        //static void Main1()
+        //{
+        //    Console.WriteLine("Welcome to main1 method");
+        //}
+
+        //static void Main()
+        //{
+        //    Console.WriteLine("Welcome to csharp Training!");
+        //    Main1();
+        //} 
+        #endregion
+
+        #region Reading and writing
+        //static void Main()
+        //{
+        //    Console.WriteLine("Please enter your name");
+
+        //    string userName = Console.ReadLine();
+
+        //    //Console.WriteLine("Hello " + userName);
+        //    // we can use placeholder for same as well.
+        //    Console.WriteLine("Hello {0}", userName);
+        //} 
+        #endregion
+    }
+}
